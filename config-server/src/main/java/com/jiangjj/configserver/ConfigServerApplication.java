169a720c0aad4304ebb7f6409361643916e712d1
jiangjj <149521920@qq.com>
2018-07-10 23:57:18 +0800

    update

---
+++ config-server/src/main/java/com/jiangjj/configserver/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.jiangjj.configserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+@RestController
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@GetMapping(value = "/message")
+	public String message(@Value("${my.message:da}") String message) {
+		return message;
+	}
+}
--- config-server/src/test/java/com/jiangjj/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/jiangjj/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package jiangjj.configserver;
+package com.jiangjj.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
